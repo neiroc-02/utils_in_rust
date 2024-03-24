dacd323b812b55081ee70bb80999ebe2137ce579
Neiro Cabrera <neiro.cabrera@yahoo.com>
2024-03-24 14:07:34 -0700

    Developing command line argument functionality and shell script to run command without cargo run

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,30 +1,48 @@
 #![allow(unused_imports)]   //To allow unused libraries
 #![allow(dead_code)]        //To allow stubs
+#![allow(unused_assignments)]
+#![allow(unused_variables)]
 use std::env::args;
 use regex::Regex;                                
 use std::fs::File;
-use colored::*;
 use walkdir::WalkDir;
-use clap::{Arg, App};
-
-/*
- Tasks to reimplement grep:
- 0. Be able to handle command line arguments
- 1. Be able to find a file in current directory and dig through the tree
- 2. Load that file
- 3. Read line by line through the file to find regex matches
- 4. Print the matches with colored matches
-*/
-fn find_directory(){
-    print!("Hello World")
-}
-
+use colored::*;
 
 fn main() {
-    let args: Vec<String> = args().skip(1).collect();
-    for elem in args {
-        print!("{} \n", elem)
+    let mut regex_pattern: String = "".to_string();
+    let mut files: Vec<String> = Vec::new(); 
+    let mut is_extended: bool = false;
+    let mut recurse_dirs: bool = false;
+    let mut track_counts: bool = false;
+    let args: Vec<String> = args().skip(1).collect(); //Gathering command line args
+    //Handling command line options
+    for arg in args {
+        if arg.starts_with("-") {
+            match &arg[1..] {
+            "e" => {
+                is_extended = true;
+            }
+            "r" => {
+                recurse_dirs = true;
+            }
+            "c" => {
+                track_counts = true
+            }
+            _ => panic!("Bad Argument!")
+            }
+        }
+        else {
+            if regex_pattern == "" {
+                match Regex::new(&arg) {
+                    Ok(_) => regex_pattern = arg,
+                    Err(_) => panic!("Invalid Regex Pattern"),
+                }
+            }
+            else {
+                files.push(arg);
+            } 
+        }
     }
-    
+    println!("Finished argument parsing without errors!");
 }
 
