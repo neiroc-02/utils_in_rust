7d292864f0aa790115c01f94b798208cdca7cd46
Neiro Cabrera <neiro.cabrera@yahoo.com>
2024-03-24 19:23:58 -0700

    Implemented basic grep functionality, added test file

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -3,32 +3,47 @@
 #![allow(unused_assignments)]
 #![allow(unused_variables)]
 use std::env::args;
+use std::io::Read;
+use std::path::Path;
 use regex::Regex;                                
 use std::fs::File;
 use walkdir::WalkDir;
 use colored::*;
 
+fn handle_grep(regex: String, path: String) -> std::io::Result<()> {
+    let re = Regex::new(&regex).unwrap();
+    let mut f = File::open(path)?;
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)?;
+    for line in contents.lines() {
+        if let Some(captures) = re.captures(line) {
+            if let Some(pattern_match) = captures.get(0) {
+                let (start, end) = (pattern_match.start(), pattern_match.end());
+                let (before, matched, after) = (&line[..start], &line[start..end], &line[end..]);
+                println!("{}{}{}", before, matched.green(), after); 
+            }
+        }
+    }
+    Ok(())
+}
+
 fn main() {
+    //Initializing variables to track...
     let mut regex_pattern: String = "".to_string();
     let mut files: Vec<String> = Vec::new(); 
     let mut is_extended: bool = false;
     let mut recurse_dirs: bool = false;
     let mut track_counts: bool = false;
-    let args: Vec<String> = args().skip(1).collect(); //Gathering command line args
-    //Handling command line options
+    let args: Vec<String> = args().skip(1).collect();
+    //-----------------------------------------------
+    //Handling command line arguments...
     for arg in args {
         if arg.starts_with("-") {
             match &arg[1..] {
-            "e" => {
-                is_extended = true;
-            }
-            "r" => {
-                recurse_dirs = true;
-            }
-            "c" => {
-                track_counts = true
-            }
-            _ => panic!("Bad Argument!")
+            "e" => is_extended = true,
+            "r" => recurse_dirs = true,
+            "c" => track_counts = true,
+            _ => panic!("Bad Argument!"),
             }
         }
         else {
@@ -39,10 +54,24 @@ fn main() {
                 }
             }
             else {
-                files.push(arg);
+                if Path::new(&arg).exists() { files.push(arg); }
+                else { panic!("Invalid file: {}", arg) }
             } 
         }
     }
-    println!("Finished argument parsing without errors!");
+    //----------------------------------------------
+    /*
+    if track_counts {
+        panic!("Unimplemented!")
+    }
+    else if recurse_dirs {
+        panic!("Unimplemented!")
+    }
+    else {
+    */
+    for file in files {
+        let _ = handle_grep(regex_pattern.clone(), file);
+    }
+    //}
 }
 
