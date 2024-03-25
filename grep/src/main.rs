0629e310e807596222454de43c0692304e1112ce
Neiro Cabrera <neiro.cabrera@yahoo.com>
2024-03-25 13:33:49 -0700

    Added grep count functionality

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,7 +1,7 @@
-#![allow(unused_imports)]   //To allow unused libraries
-#![allow(dead_code)]        //To allow stubs
-#![allow(unused_assignments)]
-#![allow(unused_variables)]
+#![allow(unused_imports)]       //To allow unused libraries
+#![allow(dead_code)]            //To allow stubs
+#![allow(unused_assignments)]   //To allow me to initialize variables
+#![allow(unused_variables)]     //To allow variable stubs
 use std::env::args;
 use std::io::Read;
 use std::path::Path;
@@ -12,7 +12,7 @@ use colored::*;
 
 fn handle_grep(regex: String, path: String) -> std::io::Result<()> {
     let re = Regex::new(&regex).unwrap();
-    let mut f = File::open(path)?;
+    let mut f = File::open(&path)?;
     let mut contents = String::new();
     f.read_to_string(&mut contents)?;
     for line in contents.lines() {
@@ -20,24 +20,37 @@ fn handle_grep(regex: String, path: String) -> std::io::Result<()> {
             if let Some(pattern_match) = captures.get(0) {
                 let (start, end) = (pattern_match.start(), pattern_match.end());
                 let (before, matched, after) = (&line[..start], &line[start..end], &line[end..]);
-                println!("{}{}{}", before, matched.yellow(), after); 
+                println!("{}:{}{}{}", path.purple(), before, matched.blue(), after); 
             }
         }
     }
     Ok(())
 }
 
+fn handle_grep_count(regex: String, path: String) -> std::io::Result<()> {
+    let re = Regex::new(&regex).unwrap();
+    let mut f = File::open(&path)?;
+    let mut contents: String = String::new();
+    let mut count: i32 = 0;
+    f.read_to_string(&mut contents)?;
+    for line in contents.lines() {
+        if re.is_match(line){
+            count = count + 1;
+        }
+    }
+    println!("{}:{}", path.purple(), count);
+    Ok(())
+}
+
 fn main() {
     let mut regex_pattern: String = "".to_string();
     let mut files: Vec<String> = Vec::new(); 
-    let mut is_extended: bool = false;
     let mut recurse_dirs: bool = false;
     let mut track_counts: bool = false;
     let args: Vec<String> = args().skip(1).collect();
     for arg in args {
         if arg.starts_with("-") {
             match &arg[1..] {
-            "e" => is_extended = true,
             "r" => recurse_dirs = true,
             "c" => track_counts = true,
             _ => panic!("Bad Argument!"),
@@ -56,8 +69,15 @@ fn main() {
             } 
         }
     }
-    for file in files {
-        let _ = handle_grep(regex_pattern.clone(), file);
+    if track_counts {
+        for file in files {
+            let _ = handle_grep_count(regex_pattern.clone(), file.clone());
+        }
     }
+    else {
+        for file in files {
+            let _ = handle_grep(regex_pattern.clone(), file.clone());
+        }
+    }   
 }
 
