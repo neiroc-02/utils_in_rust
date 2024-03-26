48c346fda321dc0247071a3bf5e4c18f4af01722
Neiro Cabrera <neiro.cabrera@yahoo.com>
2024-03-26 13:15:13 -0700

    Added directory checks and thread pool psuedocode

--- grep/src/main.rs
+++ grep/src/main.rs
@@ -1,14 +1,16 @@
 #![allow(unused_imports)]       //To allow unused libraries
 #![allow(dead_code)]            //To allow stubs
 #![allow(unused_assignments)]   //To allow me to initialize variables
-#![allow(unused_variables)]     //To allow variable stubs
-use std::env::args;
+#![allow(unused_variables)]     
+use std::{env::args, fs};
 use std::io::Read;
 use std::path::Path;
 use regex::Regex;                                
-use std::fs::File;
+use std::fs::{File, Metadata};
 use walkdir::WalkDir;
 use colored::*;
+use num_cpus;
+use rayon::prelude::*;
 
 fn handle_grep(regex: String, path: String) -> std::io::Result<()> {
     let re = Regex::new(&regex).unwrap();
@@ -42,6 +44,7 @@ fn handle_grep_count(regex: String, path: String) -> std::io::Result<()> {
     Ok(())
 }
 
+//Consider adding support for -q, -E, -n
 fn main() {
     let mut regex_pattern: String = "".to_string();
     let mut files: Vec<String> = Vec::new(); 
@@ -69,14 +72,36 @@ fn main() {
             } 
         }
     }
-    if track_counts {
-        for file in files {
-            let _ = handle_grep_count(regex_pattern.clone(), file.clone());
-        }
+    if recurse_dirs {
+        let max_threads = num_cpus::get();
+        let num_threads: usize = if files.len() > max_threads { max_threads } else { files.len() };
+        //Another thing that must be done... 
+        //if a thread encounters a directory, expand it and put it into a list of files
+/*
+        let pool = rayon::ThreadPoolBuilder::new().num_threads(num_threads).build().unwrap();
+        pool.install(|| {
+            files.par_iter().for_each(|file| {
+                if track_counts {
+                    let _ = handle_grep_count(regex_pattern.clone(), file.clone());
+                } 
+                else {
+                    let _ = handle_grep(regex_pattern.clone(), file.clone());
+                }
+            });
+        });
+*/
     }
     else {
         for file in files {
-            let _ = handle_grep(regex_pattern.clone(), file.clone());
+            let metadata = fs::metadata(&file).unwrap();
+            if metadata.is_file() {
+                if track_counts {
+                    let _ = handle_grep_count(regex_pattern.clone(), file.clone());
+                }
+                else {
+                    let _ = handle_grep(regex_pattern.clone(), file.clone());
+                }
+            }
         }
     }   
 }
